696ef2171797dbe84158bd84165d8ea00aaf09ed
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-07-20 22:17:03 +0700

    Upgrade Phoenix to 1.4
    - Change json library to Jason
    - Pin Ecto version, see: https://stackoverflow.com/a/53046779

--- lib/event_promo_code/repo.ex
+++ lib/event_promo_code/repo.ex
@@ -1,3 +1,5 @@
 defmodule EventPromoCode.Repo do
-  use Ecto.Repo, otp_app: :event_promo_code
+  use Ecto.Repo,
+    otp_app: :event_promo_code,
+    adapter: Ecto.Adapters.Postgres
 end
--- mix.exs
+++ mix.exs
@@ -33,14 +33,16 @@ defmodule EventPromoCode.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.3"},
+      {:phoenix, "~> 1.4.0"},
       {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
+      {:phoenix_ecto, ">= 3.2.0 and < 3.5.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:plug, "~> 1.7"},
+      {:jason, "~> 1.0"}
     ]
   end
 
