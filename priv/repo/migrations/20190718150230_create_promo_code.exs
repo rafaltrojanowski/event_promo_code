f2323de7cb73cc0ca507358d1a8e3f5feaba3ef2
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-07-22 19:00:37 +0700

    Add null: false constraint for mandatory fields

--- priv/repo/migrations/20190718150230_create_promo_code.exs
+++ priv/repo/migrations/20190718150230_create_promo_code.exs
@@ -4,10 +4,10 @@ defmodule EventPromoCode.Repo.Migrations.CreatePromoCode do
   def change do
     create table(:promo_codes) do
       add :event_id, references(:events), null: false
-      add :amount, :float
-      add :expires_at, :utc_datetime
+      add :amount, :float, null: false
+      add :expires_at, :utc_datetime, null: false
       add :is_active, :boolean, default: true, null: false
-      add :radius, :float
+      add :radius, :float, null: false
 
       timestamps()
     end
